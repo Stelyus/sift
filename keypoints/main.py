7fd12ef8862053f3009e6f52ffc95e60e3e8d2c9
Franck Thang <stelyus@outlook.com>
2018-11-20 22:12:45 +0100

    Own gaussian matrix function

--- keypoints/main.py
+++ keypoints/main.py
@@ -5,39 +5,46 @@ import  matplotlib.pyplot as plt
 import sys
 
 
+def laplacian_keypoints(img, std=1, show=False):
 
-def laplacian_keypoints(img):
-
-    gaussian_matrix = np.array([[1,4,6,4,1],\
-                             [4,16,24,16,4],\
-                             [6,24,36,24,6],\
-                             [4,16,24,16,4],\
-                             [1,4,6,4,1]])
-
-    gaussian_matrix = gaussian_matrix / 256
-
-    print(gaussian_matrix)
-    blurred = img
-    for _ in range(50):
-        blurred = signal.convolve2d(blurred, gaussian_matrix, 'same').astype('uint8')
-        print(blurred)
+    def gaussian_matrix(x, y, std):
+        std = 2 * (std ** 2)
+        exp_arg = (x ** 2 + y ** 2) / std
+        return (1 / (np.pi * std)) * np.exp([-exp_arg])[0]
 
 
-    #blurred = np.expand_dims(blurred, axis=-1)
-    fig = plt.figure()
-    ax = fig.add_subplot(121)
-    ax1 = fig.add_subplot(122)
-    ax.imshow(img, cmap="gray")
-    ax.set_title("Original image")
 
-    ax1.imshow(blurred, cmap="gray")
-    ax1.set_title("Blurred image")
-    plt.show()
+    kernel_shape = (5,5) 
+    mid = 2
+    my_gaussian_matrix = np.zeros(kernel_shape)
+
+    for h in range(kernel_shape[0]):
+        for w in range(kernel_shape[1]):
+            my_gaussian_matrix[h,w] = gaussian_matrix(np.abs(mid - h),np.abs(mid - w), std)
+
+    my_gaussian_matrix /= np.sum(my_gaussian_matrix)
+    blurred = signal.convolve2d(img, my_gaussian_matrix, 'same').astype('uint8')
+
+    if show:
+        fig = plt.figure()
+        ax = fig.add_subplot(121)
+        ax1 = fig.add_subplot(122)
+        ax.imshow(img, cmap="gray")
+        ax.set_title("Original image")
+
+        ax1.imshow(blurred, cmap="gray")
+        ax1.set_title("Blurred image")
+        plt.show()
+
+    return blurred
+
+
+
 
 path = '/Users/franckthang/Work/PersonalWork/sift/resources/cat.jpg'
 img = np.array(Image.open(path).convert('L'))
-print("Image shape: {!r}".format(img.shape))
+print("Image shape: {}".format(img.shape))
 
-laplacian_keypoints(img)
+laplacian_keypoints(img, std=10, show=True)
 
 
