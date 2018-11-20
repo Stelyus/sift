1effd0cc1c7b3be728c06e847c0e2ea5a19b66a8
Franck Thang <stelyus@outlook.com>
2018-11-20 23:25:35 +0100

    Did the first octave

--- keypoints/laplacian/main.py
+++ keypoints/laplacian/main.py
@@ -4,24 +4,31 @@ from scipy import signal
 import  matplotlib.pyplot as plt
 import sys
 
+show = False
 
-def laplacian_keypoints(img, std=1, show=False):
+path = '/Users/franckthang/Work/PersonalWork/sift/resources/cat.jpg'
+img = np.array(Image.open(path).convert('L'))
+print("Image shape: {}".format(img.shape))
 
+
+
+def blur(std):
+
+    print("Blur with {} value".format(std))
     def gaussian_matrix(x, y, std):
         std = 2 * (std ** 2)
         exp_arg = (x ** 2 + y ** 2) / std
         return (1 / (np.pi * std)) * np.exp([-exp_arg])[0]
 
-
-
-    kernel_shape = (5,5) 
-    mid = 2
+# Creating here the gaussian matrix
+    kernel_shape = (7, 7)
+    mid = (kernel_shape[0] - 1) / 2
     my_gaussian_matrix = np.zeros(kernel_shape)
-
     for h in range(kernel_shape[0]):
         for w in range(kernel_shape[1]):
             my_gaussian_matrix[h,w] = gaussian_matrix(np.abs(mid - h),np.abs(mid - w), std)
 
+# Normalize so that the sum is equal to 1
     my_gaussian_matrix /= np.sum(my_gaussian_matrix)
     blurred = signal.convolve2d(img, my_gaussian_matrix, 'same').astype('uint8')
 
@@ -39,12 +46,19 @@ def laplacian_keypoints(img, std=1, show=False):
     return blurred
 
 
+def scale_space():
+    octave = 4
+    pictures = 5
+    k = np.sqrt(2)
+    std = np.sqrt(5)
 
+    plt.subplot(2,3,1)
+    plt.imshow(img, cmap="gray")
 
-path = '/Users/franckthang/Work/PersonalWork/sift/resources/cat.jpg'
-img = np.array(Image.open(path).convert('L'))
-print("Image shape: {}".format(img.shape))
+    for i in range(pictures):
+        plt.subplot(2,3,i+2)
+        std *= k
+        plt.imshow(blur(std), cmap="gray")
+    plt.show()
 
-laplacian_keypoints(img, std=10, show=True)
-
-
+scale_space()
